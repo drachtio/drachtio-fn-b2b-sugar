4cae032c739c7d3085e07196761b3091c81ec214
Dave Horton <daveh@beachdognet.com>
2019-04-06 11:39:43 -0400

    pass logger (optional) in constructor

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ const noop = () => {};
 const noopLogger = {debug: noop, info: noop, error: console.error};
 const CallManager = require('./lib/call-manager');
 
-function simring(req, res, uriList, opts, notifiers) {
-  assert(req instanceof SipRequest);
-  assert(res instanceof SipResponse);
-  assert(Array.isArray(uriList));
-  const logger = opts.logger || noopLogger;
+function simring(logger) {
+  return function(req, res, uriList, opts, notifiers) {
+    assert(req instanceof SipRequest);
+    assert(res instanceof SipResponse);
+    assert(Array.isArray(uriList));
+    const logger = logger || noopLogger;
 
-  const manager = new CallManager({req, res, uriList, opts, notifiers, logger});
-  return manager.simring();
+    const manager = new CallManager({req, res, uriList, opts, notifiers, logger});
+    return manager.simring();
+  };
 }
 
 
